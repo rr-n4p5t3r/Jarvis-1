dc918239d57574287d5ae95e6f6c406e5662ecb8
Ricardo Rosero <dev@rrsolucionesit.com>
2024-05-11 13:18:35 -0500

    commit

--- src/main/java/co/com/spn/cun3/webService/ObtenerTokenResponse.java
+++ src/main/java/co/com/spn/cun3/webService/ObtenerTokenResponse.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2023.08.23 a las 07:48:55 PM COT 
+// Generado el: 2024.03.22 a las 02:38:54 PM COT 
 //
 
 
